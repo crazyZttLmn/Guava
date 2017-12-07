1be8fd69acbb8de342862fade5fc69085a633cd6
zhangtingting <593966849@qq.com>
2017-12-07 10:11:39 +0800

    增加文件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,14 @@
     <h1>{{ msg }}</h1>
     <h2>{{ title }}</h2>
     <el-button type="primary">主要按钮</el-button>
+    <el-row :gutter="60">
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
   </div>
+
 </template>
 
 <script>
@@ -11,8 +18,8 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: '这是一个神奇的网站',
-      title: '哈哈'
+      msg: '番石榴',
+      title: '梦楠，婷婷'
     }
   }
 }
@@ -33,5 +40,31 @@ li {
 }
 a {
   color: #42b983;
+}
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+     margin-bottom: 0;
+   }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: red;
+}
+.bg-purple {
+  background: lightblue;
+}
+.bg-purple-light {
+  background: cyan;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: lightcoral;
 }
 </style>
